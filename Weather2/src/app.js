0b8b0310ae254bd1f543cee0dffa5647b5f4d839
jsphLim <1806291589@qq.com>
2017-12-17 16:20:20 +0800

    Update app.js

--- Weather2/src/app.js
+++ Weather2/src/app.js
@@ -1,7 +1,6 @@
 //app.js
 App({
   onLaunch: function () {
-    //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
@@ -11,7 +10,6 @@ App({
     if(this.globalData.userInfo){
       typeof cb == "function" && cb(this.globalData.userInfo)
     }else{
-      //调用登录接口
       wx.login({
         success: function () {
           wx.getUserInfo({
